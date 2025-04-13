ebad247fdc6432bac85baee1184517f2a9ff63e1
KALKI9437 <159163635+KALKI9437@users.noreply.github.com>
2025-04-13 10:59:04 +0530

    Update index.js

--- api/server/index.js
+++ api/server/index.js
@@ -24,7 +24,7 @@ const routes = require('./routes');
 
 const { PORT, HOST, ALLOW_SOCIAL_LOGIN, DISABLE_COMPRESSION, TRUST_PROXY } = process.env ?? {};
 
-const port = Number(process.env.PORT) || 3080;
+const port = Number(process.env.PORT) || 10000;
 const host = process.env.HOST || '0.0.0.0';
 const trusted_proxy = Number(TRUST_PROXY) || 1; /* trust first proxy by default */
 
